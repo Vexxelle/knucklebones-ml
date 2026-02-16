e75d3c5bbf90f4902978be124659d77773e0306a
Arafloh <86147701+Arafloh@users.noreply.github.com>
2026-02-16 12:43:54 +0100

    Expand intergrated Test function

--- tests/integration_tests/test_core_logic_integrated.py
+++ tests/integration_tests/test_core_logic_integrated.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 from knucklebones_ml.env import core_logic as logic
 
-def test_first_turns():
+def test_manual_game():
     board = logic.get_board(3, 3)
     assert board[0].shape == (3, 3)
     assert board[1].shape == (3, 3)
@@ -25,3 +25,10 @@ def test_first_turns():
                                 [0, 0, 3]]))
     assert np.array_equal(board[0], expected_board[0])
     assert np.array_equal(board[1], expected_board[1])
+
+    assert logic.evaluate_column_score(board[0][:,1]) == 3
+    assert logic.evaluate_column_score(board[1][:,2]) == 9
+
+    board = logic.apply_action(3, board, 1, 2)
+    with pytest.raises(ValueError):
+        logic.apply_action(6, board, 1, 2)
