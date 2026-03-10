b8d3c7b5ea69185aaf4657a09d636723d5c6cb89
Vexxelle <86147701+Vexxelle@users.noreply.github.com>
2026-03-10 22:10:58 +0100

    Add observation and obseervation space

--- src/knucklebones_ml/env/knucklebones_environment.py
+++ src/knucklebones_ml/env/knucklebones_environment.py
@@ -9,12 +9,24 @@ from __future__ import annotations
 
 from collections import defaultdict
 from copy import copy
+from functools import cache
 
+import gymnasium as gym
 import numpy as np
 from pettingzoo import AECEnv
 from pettingzoo.utils import AgentSelector
 
 from . import core_logic as logic
+
+
+@cache
+def _observation_space() -> gym.spaces.Dict:
+    obs_dict: dict[str, gym.spaces.Space] = {
+        "die": gym.spaces.Discrete(6, start=1),
+        "board": gym.spaces.Box(low=0, high=6, shape=(2, 3, 3), dtype=np.int16),
+        "action_mask": gym.spaces.MultiBinary(3),
+    }
+    return gym.spaces.Dict(obs_dict)
 
 
 class raw_env(AECEnv):  # noqa: N801
@@ -71,11 +83,39 @@ class raw_env(AECEnv):  # noqa: N801
         self._agent_selector = AgentSelector(self.agents)
         self.agent_selection = self._agent_selector.next()
 
+    def observe(self, agent: str) -> dict[str, np.int_ | np.ndarray]:
+        """
+        Get the observation for the specified agent.
+
+        The observation includes:
+        - "die": The current die roll (1-6).
+        - "board": The current state of the board, adjusted for the agent's perspective.
+        - "action_mask": A binary vector indicating valid actions for
+            the current board state.
+
+        """
+        adjusted_board = self.board.copy()
+        if agent == "player_1":
+            adjusted_board = np.flip(adjusted_board, 0)
+
+        return {
+            "die": self.die,
+            "board": adjusted_board,
+            "action_mask": logic.get_valid_actions(adjusted_board)[0],
+        }
+
     def render(self):
         pass
 
-    def observation_space(self, agent):
-        return self.observation_spaces[agent]
+    def observation_space(self, agent: str = "player_0") -> gym.spaces.Dict:  # noqa: ARG002
+        """
+        Get the observation space for the specified agent.
 
-    def action_space(self, agent):
-        return self.action_spaces[agent]
+        In this implementation, the observation space is the same for both
+            agents and consists of:
+        - "die": A discrete space representing the current die roll (1-6).
+        - "board": A 3D box space representing the 3x3 board state for both players.
+        - "action_mask": A binary vector indicating valid actions for the
+            current board state.
+        """
+        return _observation_space()
