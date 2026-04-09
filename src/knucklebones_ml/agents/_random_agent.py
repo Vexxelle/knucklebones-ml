3177d530e2cc286231aceefacd408b8d27e7c7dd
Vexxelle <86147701+Vexxelle@users.noreply.github.com>
2026-04-09 23:57:09 +0200

    Add Agents Base Class and Random Agent

---
+++ src/knucklebones_ml/agents/_random_agent.py
@@ -0,0 +1,18 @@
+from typing import Any, Literal
+
+from numpy.random import default_rng
+
+from knucklebones_ml.agents._base_class import BaseAgent
+
+
+class RandomAgent(BaseAgent):
+    def __init__(self, seed: int | None = None) -> None:
+        self.rng = default_rng(seed)
+
+    def select_action(self, observation: dict[str, Any]) -> Literal[0, 1, 2]:
+
+        action_mask = observation["action_mask"]
+        actions = action_mask.nonzero()
+        action = self.rng.choice(actions)
+
+        return action
