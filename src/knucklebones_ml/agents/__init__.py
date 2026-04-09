c11b764c84a5ab76afa4c568f7d74022ced707a7
Vexxelle <86147701+Vexxelle@users.noreply.github.com>
2026-04-09 23:57:09 +0200

    Add Agents Base Class and Random Agent

---
+++ src/knucklebones_ml/agents/__init__.py
@@ -0,0 +1,6 @@
+"""Agents for the Knucklebones environment."""
+
+from knucklebones_ml.agents._base_class import BaseAgent
+from knucklebones_ml.agents._random_agent import RandomAgent
+
+__all__ = ["BaseAgent", "RandomAgent"]
---
+++ src/knucklebones_ml/agents/_base_class.py
@@ -0,0 +1,10 @@
+from typing import Any, Literal
+
+
+class BaseAgent:
+    def __init__(self, seed: int | None = None) -> None:
+        pass
+
+    def select_action(self, observation: dict[str, Any]) -> Literal[0, 1, 2]:
+        msg = "select_action method must be implemented by Agents"
+        raise NotImplementedError(msg)
