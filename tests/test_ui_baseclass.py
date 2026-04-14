c33a7d01cef2a8b4e9ae64812b400a3e21d9dc00
Arafloh <86147701+Arafloh@users.noreply.github.com>
2026-04-14 23:32:27 +0200

    Rename UI and Agent Base Classes

--- tests/test_ui_baseclass.py
+++ tests/test_ui_baseclass.py
@@ -1,17 +1,17 @@
 import numpy as np
 import pytest
 
-from knucklebones_ml.ui import BaseUI
+from knucklebones_ml.ui import UserInterface
 
 
 def test_same_player_names():
     error_message = "Player names must be unique."
     with pytest.raises(ValueError, match=error_message):
-        BaseUI(players=("Alice", "Alice"))
+        UserInterface(players=("Alice", "Alice"))
 
 
 def test_human_action_not_implemented():
-    ui = BaseUI(players=("Alice", "Bob"))
+    ui = UserInterface(players=("Alice", "Bob"))
     error_message = "The get_human_action method needs to be implemented by subclasses."
     with pytest.raises(NotImplementedError, match=error_message):
         ui.get_human_action(player="Alice", action_mask=np.array([1, 1, 1]))
